296e9450300ca8b550d6a202694b2c04f23876df
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-17 02:45:37 +0100

    Initialize route with react-router-dom to Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/Dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
