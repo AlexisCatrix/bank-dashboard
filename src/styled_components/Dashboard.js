9aaafaf391936815da1fce2773551cf96284dbd2
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-17 02:47:13 +0100

    Add some style for welcome title

---
+++ src/styled_components/Dashboard.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DashboardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const WelcomeTitle = styled.h1`
+  font-size: 1.5em;
+  font-weight: bold;
+`;
