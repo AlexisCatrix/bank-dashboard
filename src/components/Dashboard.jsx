689eaf78a919a6a056d6e43f11c82c8964d36752
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-17 02:47:45 +0100

    Write a welcome title for futur dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import {
+  DashboardContainer,
+  WelcomeTitle,
+} from "../styled_components/Dashboard";
+
+export default function Dashboard() {
+  return (
+    <DashboardContainer>
+      <WelcomeTitle>Hi there ! There will be a dashboard here !</WelcomeTitle>
+    </DashboardContainer>
+  );
+}
