3f332289e3c0dbc07086dfcd9be4a0006beaec71
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-17 02:46:36 +0100

    Initialize router to App and set styled-reset

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Reset } from "styled-reset";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Reset />
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
