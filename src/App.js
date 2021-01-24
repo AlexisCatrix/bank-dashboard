3c1361e860d290898c927ad3c3c3e401ceb03bde
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-24 23:50:50 +0100

    Create a route to work in sandbox environment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import InputData from "./components/InputData";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={InputData} />
         <Route path="/Dashboard" component={Dashboard} />
       </Switch>
     </Router>
