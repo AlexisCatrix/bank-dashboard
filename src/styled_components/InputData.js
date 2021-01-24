de38a9e374cd9878a823211156eea064a3a11cea
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-24 23:43:40 +0100

    Create basic style for testing inputs in sandbox environment

---
+++ src/styled_components/InputData.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const InputDataContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Information = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 45vw;
+  height: auto;
+  border: 2px solid red;
+  margin: 2vw;
+`;
+
+export const Label = styled.h1`
+  font-size: 1em;
+  margin: 1.5vh;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Submit = styled.input`
+  margin: 10vh 0vh;
+`;
+
+export const InputField = styled.input`
+margin: 2vh;
+`;
