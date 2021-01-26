30bc11e4747d698b4db88e5dd73b6544c72edd39
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-26 05:48:28 +0100

    Add some style => very basic

--- src/styled_components/Dashboard.js
+++ src/styled_components/Dashboard.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { ReturnUpBack } from "@styled-icons/ionicons-solid/ReturnUpBack";
 
 export const DashboardContainer = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
   width: 100vw;
   height: 100vh;
@@ -11,4 +13,21 @@ export const DashboardContainer = styled.div`
 export const WelcomeTitle = styled.h1`
   font-size: 1.5em;
   font-weight: bold;
+`;
+
+export const HomeIcon = styled(ReturnUpBack)`
+  color: black;
+  width: 10vh;
+`;
+
+export const Header = styled.div`
+display: flex;
+justify-content: center;
+  height: 8vh;
+  width: 100vw;
+  border: 2px solid black;
+`;
+
+export const WorkPlace = styled.div`
+  height: 80vh;
 `;
--- src/styled_components/InputData.js
+++ src/styled_components/InputData.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 
 export const InputDataContainer = styled.div`
-  width: 100vw;
+  width: 95vw;
   height: 100vh;
+  margin: 5vh auto 0 auto;
 `;
 
 export const Information = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  width: 45vw;
+  width: inherit;
   height: auto;
   border: 2px solid red;
-  margin: 2vw;
 `;
 
 export const Label = styled.h1`
@@ -36,4 +38,26 @@ export const InputField = styled.input`
 
 export const ActionButtons = styled.div`
   display: flex;
+`;
+
+export const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Header = styled.div`
+  width: 100vw;
+  height: 10vh;
+  background: #154360;
+  display: flex; 
+  align-items: center;
+`;
+
+export const NavItem = styled(Link)`
+  color: white;
+  text-decoration: none;
+  padding: 2vw;
 `;
