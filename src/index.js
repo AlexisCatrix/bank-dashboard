c1012d31ea4b2443e812b78f259a9c423b95d9cb
Alexis Catrix <alexis@ylly.fr>
2021-12-19 23:31:27 +0100

    Remove reset styled components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Reset } from "styled-reset";
+import { BrowserRouter as Router} from "react-router-dom";
+import "./assets/style/sass/main.scss";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Reset />
       <App />
     </Router>
   </React.StrictMode>,
